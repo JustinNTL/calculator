1fd2213f6440a060debdf243a7946f0b4bb86065
Justin <Justin.nthai@gmail.com>
2022-02-25 18:21:30 -0800

    Add header, calculator display, digits, operators, keys and corresponding data key numbers to index file, Create operate function to call basic arithmetic functions and take numbers in js

--- js/calculator.js
+++ js/calculator.js
@@ -3,4 +3,21 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
-const operate = (operator, ) => {};
+const operate = (operator, num1, num2) => {
+  // if operator = a/s/m/d call function, probably pop a reduction method to 
+  // receive infinite number of digits
+  switch (operator) {
+    case add:
+      return add(num1, num2);
+      break;
+    case subtract:  
+      return subtract(num1, num2);
+      break;
+    case multiply: 
+      return multiply(num1, num2);
+      break;
+    case divide:
+      return divide(num1, num2);
+      break;
+  }
+};
