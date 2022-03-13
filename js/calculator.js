b70bba39d90c1d2871c7c0582ad182589cc10c3c
Justin <Justin.nthai@gmail.com>
2022-03-13 16:05:15 -0700

    Reposition html button elements for intuitivity, Set Calculator class base to streamline and organize js

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,42 @@
+class Calculator {
+  constructor() {
+
+  }
+
+  clear() {
+
+  }
+
+  percent(number) {
+
+  }
+
+  backspace() {
+
+  }
+
+  operation(operator) {
+
+  }
+
+  negate(number) {
+
+  }
+
+  appendNumber(number) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+
+  }
+
+}
+
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
